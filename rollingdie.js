5ea818bd9c4bb29499ce0cbe138a299707e05059
EC2 Default User <ec2-user@ip-172-31-11-15.ap-southeast-2.compute.internal>
2021-05-13 07:44:09 +0000

    add javascript

---
+++ rollingdie.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+var Die = new Array(6);
+Die[1] = 0;
+Die[2] = 2;
+Die[3] = 4;
+Die[4] = 6;
+Die[5] = 8;
+Die[0] = 100;
+var n = (Math.floor(Math.random() * 6));
+var m = (Math.floor(Math.random() * 6));
+console.log(Die[n]);
+console.log(Die[m]);
